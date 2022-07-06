721d3bd93bd0401eb5cee29ac286776b095c1bcf
MahdiAmini98 <81927118+mahdiamini98@users.noreply.github.com>
2022-07-06 19:39:13 +0430

    Added Virtual Proxsy Example.

---
+++ Example 1/Proxsy/Proxsy.cs
@@ -0,0 +1,22 @@
+﻿using ProxyPattern.Example_1.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern.Example_1.Proxsy
+{
+    public class Proxsy : ISubject
+    {
+        private Subject _subject;
+        public void DoAction()
+        {
+            if (_subject == null)
+            {
+                _subject = new Subject();
+            }
+            _subject.DoAction();
+         }
+    }
+}
---
+++ Example 1/SubjectService/ISubject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern.Example_1.Interface
+{
+    public interface ISubject
+    {
+        void DoAction();
+    }
+
+
+    public class Subject : ISubject
+    {
+        public void DoAction()
+        {
+            Console.WriteLine("ISubject.Subject.DoAction ........Run");
+        }
+    }
+
+}
